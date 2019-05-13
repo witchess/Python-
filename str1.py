0ee35b6ff930e43490a7acb723bb76bd0f677be9
yangyanfang <1822980791@qq.com>
2019-05-13 23:11:13 +0800

    返回列表中重复次数最多的元素--方法1

---
+++ str1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#-*- coding utf-8 -*-
+
+"""返回列表中出现次数最多的元素"""
+from collections import Counter
+
+
+def max_list(lt):
+	temp = 0 
+	for i in lt:
+		if lt.count(i) > temp:
+			max_str = i
+			temp = lt.count(i)
+		return max_str
+
+
+
+if __name__ == '__main__':
+	fn1 = max_list(["mary","mary","mary",1,1,2])
+	
