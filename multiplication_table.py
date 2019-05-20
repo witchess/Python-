72c245c577865a8a8ffc061f846e73d544132202
yangyanfang <1822980791@qq.com>
2019-05-20 20:19:46 +0800

    乘法表的不同表示格式

--- multiplication_table.py
+++ multiplication_table.py
@@ -7,15 +7,65 @@
 
 
 def fun1():
-    print("\n".join("\t".join(["%s * %s = %s" % (x, y ,x*y) for y in range(1,x+1)]) for x in range(1,10)))
-
+	"""一行代码实现乘法表"""
+	print("\n".join("\t".join(["{}*{} = {:2}".format(x, y, x*y) for y in range(1,x+1)])for x in range(1,10)))
+    
 
 def fun2():
+	"""乘法表的常规表示---左上三角"""
 	for i in range(1,10):
 		for j in range(1,i+1):
-			print('{} * {} = {}\t'.format(j,i,i*j), end='')
+			print('%d*%d = %2s\t' % (j,i,i*j), end='')
 		print()
 
-fun1()
-fun2()
+
+def fun3():
+	"""长方形完整格式"""
+	for i in range(1,10):
+		for j in range(1,10):
+			print("{}*{} = {:2}\t".format(i, j, i*j), end='')
+		print("")
+
+
+def fun4():
+	"""左上三角格式"""
+	for i in range(1,10):
+		for j in range(i,10):
+			print("{}*{} = {:2}\t".format(i, j, i*j), end ='')
+		print("")
+
+
+def fun5():
+	"""右上三角格式"""
+	for i in range(1,10):
+		for k in range(1,i):
+			print(end="         ")
+		for j in range(i,10):
+			print("{}*{} = {:2}".format(i, j, i*j), end =' ')
+		print("")
+
+
+def fun6():
+	"""右上三角格式"""
+	for i in range(1,10):
+		for k in range(i,10):
+			print(end="         ")
+		for j in range(1,i):
+			print("{}*{} = {:2}".format(i, j, i*j),end=' ')
+		print("")
+
+
+def main():
+	f1 = fun1()
+	f2 = fun2()
+	f3 = fun3()
+	f4 = fun4()
+	f5 = fun5()
+	f6 = fun6()
+
+
+if __name__ == '__main__':
+	main()
+
+
 
